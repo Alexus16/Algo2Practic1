844c3907ffc6e3d245e9b1c30e491504dd174ce2
Alexus16 <zagulin2012@gmail.com>
2023-09-09 11:51:38 +0300

    p4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double currentEl = 0.0;
+        for(int curIt = 1; curIt <= 10; curIt++)
+        {
+            currentEl += 1.0/curIt;
+            System.out.print(String.format("%.4f ", currentEl));
+        }
     }
 }
