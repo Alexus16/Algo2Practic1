0efa7bd27d624fb8123004f3363f8b52d13e0c2c
Alexus16 <zagulin2012@gmail.com>
2023-09-09 11:46:52 +0300

    p2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] mas = {1, 2, 3, 4, 5};
+        int it = 0, minEl = Integer.MAX_VALUE, maxEl = Integer.MIN_VALUE, sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        while(it < mas.length)
+        {
+            mas[it] = scanner.nextInt();
+            minEl = Integer.min(minEl, mas[it]);
+            maxEl = Integer.max(maxEl, mas[it]);
+            sum += mas[it];
+        }
+        System.out.println("Sum: " + sum + " Min: " + minEl + " Max: " + maxEl);
     }
 }
