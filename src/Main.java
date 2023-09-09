e9c3949b9663205566290bff729462c47eabb043
Alexus16 <zagulin2012@gmail.com>
2023-09-09 11:52:55 +0300

    p5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(fact(new Scanner(System.in).nextInt()));
+    }
+
+    private static int fact(int a)
+    {
+        int res = 1;
+        for(int i = 1; i <= a; i++)
+        {
+            res *= i;
+        }
+        return res;
     }
 }
