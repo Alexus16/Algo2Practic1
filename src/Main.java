25b41c8f56b449a7b2787daa44ad335947f0fccc
Alexus16 <zagulin2012@gmail.com>
2023-09-09 11:43:20 +0300

    p1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int[] mas = {1, 2, 3, 4};
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < mas.length; i++)
+        {
+            sum += (mas[i] = scanner.nextInt());
+        }
+        System.out.println("Sum: " + sum + " Avg: " + sum/mas.length);
     }
 }
